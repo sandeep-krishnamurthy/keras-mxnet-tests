4ef7c603d3568c22b6d60d46e91f5e0f6cc3af25
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2017-07-03 12:18:46 -0700

    Fix mxnet path issue

--- keras1.2/run_tests.sh
+++ keras1.2/run_tests.sh
@@ -11,7 +11,7 @@ pip install --upgrade pip --user
 pip install --upgrade six --user
 
 # Install MXNet
-cd ../mxnet
+cd mxnet
 make clean
 make -j $(nproc) USE_OPENCV=1 USE_BLAS=atlas USE_CUDA=1 USE_CUDA_PATH=/usr/local/cuda USE_CUDNN=1
 cd python

324a09a73a353132e17e68cbdb7575ffe7497465
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2017-07-04 18:42:28 -0700

    Install hd5

--- keras1.2/run_tests.sh
+++ keras1.2/run_tests.sh
@@ -11,7 +11,7 @@ pip install --upgrade pip --user
 pip install --upgrade six --user
 
 # For model load and save functionality
-apt-get install python-h5py
+sudo apt-get install python-h5py
 
 # Install MXNet
 cd mxnet
